4faa62977edce7d24fe90075491a03388a221e4f
邹永赫 <1259085392z@gmail.com>
2022-04-15 10:34:45 +0800

    ADD: version

--- pkg/version/base.go
+++ pkg/version/base.go
@@ -1,10 +1,12 @@
 package version
 
-import "time"
+import (
+	"time"
+)
 
 var (
-	gitTag       string    = ""
-	gitCommit    string    = "$Format:%H$"    // sha1 from git, output of $(git rev-parse HEAD)
+	gitTag       string    = getLastGitTag()
+	gitCommit    string    = getLastCommit()  // sha1 from git, output of $(git rev-parse HEAD)
 	gitTreeState string    = "not a git tree" // state of git tree, either "clean" or "dirty"
 	buildDate    time.Time = time.Now()       // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
 )
--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,12 @@ package version
 
 import (
 	"fmt"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/object"
+	"log"
 	"runtime"
+	"strings"
 	"time"
 )
 
@@ -32,4 +37,71 @@ func Get() Info {
 		Compiler:     runtime.Compiler,
 		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 	}
+}
+
+// GetGitInfo returns last git commit information.
+func getLastCommit() string {
+	r, _ := git.PlainOpen(".")
+
+	// 获取 HEAD 指向的分支
+	ref, _ := r.Head()
+
+	// 获取由 ref 指向的提交对象
+	commit, err := r.CommitObject(ref.Hash())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	/*	repository, _ := git.PlainOpen(".")
+		headRef, err := repository.Head()
+		if err != nil {
+			log.Fatal(err)
+		}
+		headSha := headRef.Hash().String()
+
+		return headSha*/
+	commitMessage := commit.Message
+	commitMessage = strings.Replace(commitMessage, "\n", "", -1)
+	return commitMessage
+}
+
+// getGitTag returns the git tag.
+func getLastGitTag() string {
+	repository, _ := git.PlainOpen(".")
+	tagRefs, err := repository.Tags()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var lastTagCommit *object.Commit
+	var lastTagName string
+	err = tagRefs.ForEach(func(tagRef *plumbing.Reference) error {
+		revision := plumbing.Revision(tagRef.Name().String())
+		tagCommitHash, err := repository.ResolveRevision(revision)
+		if err != nil {
+			return err
+		}
+
+		commit, err := repository.CommitObject(*tagCommitHash)
+		if err != nil {
+			return err
+		}
+
+		if lastTagCommit == nil {
+			lastTagCommit = commit
+			lastTagName = tagRef.Name().String()
+		}
+
+		if commit.Committer.When.After(lastTagCommit.Committer.When) {
+			lastTagCommit = commit
+			lastTagName = tagRef.Name().String()
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return lastTagName
 }
